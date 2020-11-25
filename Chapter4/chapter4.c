e7192caf1f5d648a8a02f67bf1517b6f9b463ad4
Shannon Chen <shannc6@cs.washington.edu>
2020-11-24 17:33:05 -0800

    Set up chapter4 files

---
+++ Chapter4/chapter4.c
@@ -0,0 +1,25 @@
+//
+// Created by Shannon Chen on 11/24/20.
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/* 4.1 printf */
+int fourPointOne(void)
+{
+    printf("**** 4.1 output ****\n");
+    int a = 2;
+    int b = 4;
+    printf("I have %d dogs and %d cats\n", a, b);
+    return 0;
+}
+
+
+
+int main(void) {
+    fourPointOne();
+}
+
+
+
